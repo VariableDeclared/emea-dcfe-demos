2644d3ad3c3dc3480b21ea56621fbd97c5195fef
Peter Jose De Sousa <peter.de.sousa@canonical.com>
2024-07-16 21:42:01 +0100

    Microcloud init fixes

--- microcloud-deploy/variables.tf
+++ microcloud-deploy/variables.tf
@@ -13,20 +13,20 @@ variable "bridge_nic" {
 }
 variable "lookup_subnet" {
     type = string
-    default = "10.10.32.0/24"
+    default = "10.10.32.1/24"
 }
 
 variable "ovn_gateway" {
     type = string
-    default = "10.10.32.1/24"
+    default = "192.168.254.1/24"
 }
 variable "ovn_range_start" {
     type = string
-    default = "10.10.32.150"
+    default = "192.168.254.150"
 }
 variable "ovn_range_end" {
     type = string
-    default = "10.10.32.200"
+    default = "192.168.254.200"
 }
 variable "ssh_pubkey" {
     type = string
